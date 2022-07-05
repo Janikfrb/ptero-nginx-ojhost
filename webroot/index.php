eef58af8e3fb6f345889c054de127b7ffe78d84e
Janik <77326678+Janikfrb@users.noreply.github.com>
2022-07-05 12:47:19 +0200

    Create index.php

---
+++ webroot/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+    <html>
+    <head>
+    <title>oneJanik Hosting - Default Page</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=0.5">
+    </head>
+    <body>
+    <header>
+    <h1><b>oneJanik Hosting</b></h1>
+    </header><br><br>
+    <h3>Hurra! Deine Seite ist fertig eingerichtet.</h3><br>
+<br><h5>Deine Nächsten Schritte:</h5><br>
+ <p>1. lade jetzt einfach deine Website-Dateien in den Ordner <b>webroot</b> hoch</p>
+    <p>2. öffne deine Website, und überprüfe ob alles stimmt wie du es haben willst.</p>
+    <p>3. Teile deine Website mit anderen.</p>
+    </body>
+    </html>

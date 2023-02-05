6ba55144ad646ee565ad6a49387f2da5a231a4c5
Janik <77326678+Janikfrb@users.noreply.github.com>
2023-02-05 19:33:56 +0100

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,18 +1,39 @@
 <!DOCTYPE html>
     <html>
     <head>
-    <title>oneJanik Hosting - Default Page</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Lighthouse Hosting - Default Page</title>
+   
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <meta name="viewport" content="width=device-width, initial-scale=0.5">
     </head>
-    <body>
-    <header>
-    <h1><b>Lighthouse Hosting</b></h1>
-    </header><br><br>
-    <h3>Hurra! Deine Seite ist fertig eingerichtet.</h3><br>
-<br><h5>Deine Nächsten Schritte:</h5><br>
- <p>1. lade jetzt einfach deine Website-Dateien in den Ordner <b>webroot</b> hoch</p>
-    <p>2. öffne deine Website, und überprüfe ob alles stimmt wie du es haben willst.</p>
-    <p>3. Teile deine Website mit anderen.</p>
+    <body data-bs-theme="dark">
+    <nav class="navbar bg-transparent">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+    <img src="https://cdn.discordapp.com/icons/993814042546294897/e1991f3ede1274633bf00560ad41c916.webp" width="50" height="50" class="rounded-3">
+      Lighthouse Hosting
+    </a>
+  </div>
+</nav>
+        
+ <div class="text-light text-center position-absolute bottom-50 start-50 translate-middle-x">
+ <div class="card">    
+ <div class="card-title"><h1>Yay!</h1></div><br>
+    <div class="card-subtitle"> <h4 style="
+    background: rgb(93,60,1);
+background: radial-gradient(circle, rgba(93,60,1,1) 8%, rgba(37,6,110,1) 48%, rgba(0,44,184,1) 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;">Your webspace is ready!</h4></div>
+    <div class="card-body">
+     <p>Now go to your <b>webroot</b> folder and upload your files :)</p>
+</div>
+ </div>
+</div>
+     <footer class="position-absolute bottom-0 start-50 translate-middle-x">
+        <a href="https://discord.com/invite/zRbKpeHUAu"><p>Have any questions?</p></a>
+     </footer>
+         
+   
     </body>
     </html>
